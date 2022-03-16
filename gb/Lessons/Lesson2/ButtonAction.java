78799ea93e707d09fec1c96552570a99342739af
Ivan B <ivar110kg@gmail.com>
2022-03-16 11:55:23 +0400

    add ButtonAction.java

---
+++ gb/Lessons/Lesson2/ButtonAction.java
@@ -0,0 +1,12 @@
+package Lessons.Lesson2;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonAction implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(1);
+    }
+}
