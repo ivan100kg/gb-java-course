bc27b96f3790092a05d04e203a343a601feba888
Ivan B <ivar110kg@gmail.com>
2022-03-18 13:52:22 +0400

    add BoxWithNumbers.java

---
+++ gb/lessons/lesson4/BoxWithNumbers.java
@@ -0,0 +1,21 @@
+package lessons.lesson4;
+
+public class BoxWithNumbers<N extends Number> {
+    private N[] array;
+
+    public BoxWithNumbers(N... array) {
+        this.array = array;
+    }
+
+    public double average() {
+        double avg = 0.0;
+        for (int i = 0; i < array.length; i++) {
+            avg += array[i].doubleValue();
+        }
+        return avg / array.length;
+    }
+
+    public boolean compareAverage(BoxWithNumbers<?> another) {
+        return Math.abs(this.average()) - another.average() < 0.0001;
+    }
+}
