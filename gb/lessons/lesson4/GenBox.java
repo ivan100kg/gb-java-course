2bedff4bf3b57934a29e094b5e89964335b15226
Ivan B <ivar110kg@gmail.com>
2022-03-18 13:52:38 +0400

    add GenBox.java

---
+++ gb/lessons/lesson4/GenBox.java
@@ -0,0 +1,19 @@
+package lessons.lesson4;
+
+public class GenBox<T> {
+    T obj;
+
+    public GenBox(T obj) {
+        this.obj = obj;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+
+
+}
