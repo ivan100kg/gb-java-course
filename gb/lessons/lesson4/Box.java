29dcf8803df880fa3ff6bee61c89be6c2a6a5a26
Ivan B <ivar110kg@gmail.com>
2022-03-18 13:51:56 +0400

    add Box.java

---
+++ gb/lessons/lesson4/Box.java
@@ -0,0 +1,14 @@
+package lessons.lesson4;
+
+public class Box implements Comparable<Box> {
+    private int size;
+
+    public Box(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public int compareTo(Box o) {
+        return this.size - o.size;
+    }
+}
