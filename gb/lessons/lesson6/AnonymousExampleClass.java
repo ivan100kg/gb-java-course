2a625ea8b7017106070a5d4ce176d2a5fcd0d06d
Ivan B <ivar110kg@gmail.com>
2022-03-26 18:04:55 +0400

    add AnonymousExampleClass.java

---
+++ gb/lessons/lesson6/AnonymousExampleClass.java
@@ -0,0 +1,27 @@
+package lessons.lesson6;
+
+public class AnonymousExampleClass {
+    public static void main(String[] args) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("thread-" + i);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("END");
+    }
+}
