5ab4e182c77d611b22c5660862a271ac516a723a
Ivan B <ivar110kg@gmail.com>
2022-06-06 09:21:12 +0400

    Update MainApp.java

--- gb/lessons/lesson9/MainApp.java
+++ gb/lessons/lesson9/MainApp.java
@@ -3,7 +3,9 @@ package lessons.lesson9;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
@@ -17,10 +19,33 @@ public class MainApp {
 
         Field field = classCat.getDeclaredField("privateField");
         field.setAccessible(true);
+        System.out.println("cat.privateField = " + field.get(cat));
         field.set(cat, 10);
+        System.out.println("cat.privateField = " + field.get(cat));
 
         System.out.println(field.getModifiers());
         System.out.println(method.getModifiers());
+
+        System.out.println("==============================");
+
+        List<Method> executionList = new ArrayList<>();
+        Class testClass = TestClass.class;
+        Method[] methods = testClass.getDeclaredMethods();
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(MyAnnotation.class)) {
+                executionList.add(m);
+            }
+        }
+
+        executionList.sort((a,b) -> b.getAnnotation(MyAnnotation.class).priority() -
+                a.getAnnotation(MyAnnotation.class).priority());
+       executionList.forEach(method1 -> {
+           try {
+               method1.invoke(null);
+           } catch (IllegalAccessException | InvocationTargetException e) {
+               e.printStackTrace();
+           }
+       });
 
     }
 
