d40c3c5b257acad02afc67b6721d8656f1b7afce
Ivan B <ivar110kg@gmail.com>
2022-06-06 09:20:16 +0400

    Add file Cat.java

---
+++ gb/lessons/lesson9/Cat.java
@@ -0,0 +1,24 @@
+package lessons.lesson9;
+
+public class Cat {
+    private int privateField;
+    int defaultField;
+    public int publicField;
+
+    public Cat(int privateField, int defaultField, int publicField) {
+        this.privateField = privateField;
+        this.defaultField = defaultField;
+        this.publicField = publicField;
+    }
+
+    public Cat() {
+    }
+
+    public void publicMeow() {
+        System.out.println("public Meow");
+    }
+
+    private void privateMeow() {
+        System.out.println("private Meow");
+    }
+}
